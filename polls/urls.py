4de2b5ae29eed8151164f2840e19332a4a8d7e32
AlMarley <alde-nor@hotmail.com>
2020-09-22 17:53:56 -0300

    Parte 3

--- polls/urls.py
+++ polls/urls.py
@@ -4,8 +4,10 @@ from . import views
 
 
 urlpatterns = [
-     path('', views.index, name="index"),
-     path('', views.sobre, name="sobre"),
+     path('', views.index, name='index'),
+     path('<int:question_id>', views.detail, name='detail'),
+     path('<int:question_id>/result', views.mvotos, name='mvotos'),
+     path('', views.sobre, name='sobre'),
 
 
 
--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,54 @@
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
+from polls.models import Question
 
 
+def index(request, template_name='polls/index.html'):
+    latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return render(request, template_name, context)
 
-def index(request):
-    print('Minha Primeira View!')
-    return HttpResponse('Hello, World')
+
+'''    
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+
+    }
+
+    html = template.render(context, request)
+
+    #print(latest_question_list)
+    #print('Minha Primeira View!'
+
+    return HttpResponse(html)
+'''
+
+
+def detail(request, question_id, template_name='polls/detail.html'):
+    question = get_object_or_404(Question, id=question_id)
+    context = {
+        "question": question,
+    }
+    return render(request, template_name, context)
+
+
+def mvotos(request, question_id, template_name='polls/mvotos.html'):
+    question = get_object_or_404(Question, id=question_id)
+    context = {
+        "question": question,
+    }
+    return render(request, template_name, context)
 
 def sobre(request):
     print('sobre!')
     return HttpResponse('<br><b>Equipe:</b><br> <b>1:</b> Aldenor Júnior Torres Verçosa<br><b>2:</b> Deus')
+
+
+
+'''
+
+'''
